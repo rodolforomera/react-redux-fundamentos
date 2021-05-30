2e0b13e780ee41fad97cc8c8596d3ccc714e7b22
rodolforomera <rodolforomeras@gmail.com>
2021-05-30 14:32:47 -0300

    Inicio Fundamentos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const el = document.getElementById('root')
+const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        { tag }
+    </div>, 
+    el
+)
