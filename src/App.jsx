3c97456682cd3b2b7d0daf34c5b48020baa31097
rodolforomera <rodolforomeras@gmail.com>
2021-06-01 21:45:38 -0300

    Componente com Filho, Repetição, Desafio Repetição e Renderização condicional

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import './App.css';
 import React from 'react';
 
 import Card from './components/layout/Card';
+
+import UsuarioInfo from './components/condicional/UsuarioInfo';
+import ParOuImpar from './components/condicional/ParOuImpar';
+import TabelaProdutos from './components/repeticao/TabelaProdutos';
+import ListaAlunos from './components/repeticao/ListaAlunos';
+import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro';
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
@@ -13,6 +20,28 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#08 - Renderização Condicional" color="#982395">
+               <ParOuImpar numero={20}></ParOuImpar>
+               <UsuarioInfo usuario={{ nome: 'Rodolfo'}} />
+               <UsuarioInfo usuario={{ nome: ''}} />
+            </Card>
+
+            <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+               <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+               <ListaAlunos></ListaAlunos>
+            </Card>
+
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Pedro"/>
+                    <FamiliaMembro nome="Ana"/>
+                    <FamiliaMembro nome="Gustavo"/>
+                </Familia>
+            </Card>
 
             <Card titulo="#04 - Desafio Alatório" color="#FA6900">
                 <Aleatorio min={10} max={60}/>
